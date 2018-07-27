f0c523e2fcb5975a74ec76793d9d00411f27e820
weichuan <weichuan.tran@gmail.com>
2018-07-27 11:31:51 +0200

    ajout jquery + design refait

---
+++ script.js
@@ -0,0 +1,11 @@
+/* $("#GoAbout").click(function(){
+    $('html,body').animate({
+        scrollTop: $("#About").offset().top},
+    'slow');
+});
+
+$("#GoSkills").click(function(){
+    $('html,body').animate({
+        scrollTop: $("#Skill").offset().top},
+    'slow');
+}) */
